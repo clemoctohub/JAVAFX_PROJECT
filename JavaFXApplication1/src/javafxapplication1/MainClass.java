4293325748cf30457d909db5e0c7d85d4b3c1357
clemoctohub <clemfages@gmail.com>
2020-11-16 17:07:35 +0100

    start display connection tab

--- JavaFXApplication1/src/javafxapplication1/MainClass.java
+++ JavaFXApplication1/src/javafxapplication1/MainClass.java
@@ -5,19 +5,37 @@
  */
 package javafxapplication1;
 
+import com.sun.javafx.scene.layout.region.BackgroundFill;
+import static java.awt.Color.*;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.Panel;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import static java.awt.Color.red;
+import static java.awt.Color.yellow;
+import java.awt.Rectangle;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.*;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.text.Text;
+import static javax.swing.text.StyleConstants.Background;
 
 /**
  *
@@ -34,7 +52,7 @@ public class MainClass extends Application {
         Tab tab2 = new Tab("CinePass");
         Tab tab3 = new Tab("Search");
         Tab tab4 = new Tab("Connection");
-
+        tab4.setContent(getPane());
         tabPane.getTabs().add(tab1);
         tabPane.getTabs().add(tab2);
         tabPane.getTabs().add(tab3);
@@ -49,6 +67,59 @@ public class MainClass extends Application {
         primaryStage.setScene(scene);
         primaryStage.setFullScreen(true);
         primaryStage.show();
+    }
+    
+    public BorderPane getPane(){
+        BorderPane pane = new BorderPane();
+        //BorderPane p = new BorderPane();
+        //p.setCenter(getGridtab4());
+        
+        pane.setCenter(getGridtab4());
+        return pane;
+    }
+    
+    public GridPane getGridtab4(){
+        GridPane root=new GridPane(); 
+        Label Email = new Label("Your ID number :");  
+        Label Password = new Label("Password :");  
+        Email.setId("label-tab4");
+        Password.setId("label-tab4");
+//Adding text-field to the form   
+        TextField tf1=new TextField();
+        TextField tf2=new TextField(); 
+        
+        tf1.setId("text-tab4");
+        tf2.setId("text-tab4");
+        
+//creating submit button   
+        Button Submit=new Button ("Submit");  
+  
+//setting ID for the submit button so that the particular style rules can be applied to it.   
+        Submit.setId("submit"); 
+//Creating reset button   
+        Button Reset=new Button ("Reset");  
+  
+//Creating title   
+        Label title = new Label();  
+        title.setText("Sign Up"); 
+        title.setUnderline(true);  
+        title.setId("title");  
+//creating grid-pane  
+  
+//adding the the nodes to the GridPane's rows   
+        
+        root.addRow(0, title);
+        root.addRow(3, Email);
+        root.addRow(4, tf1);
+        root.addRow(6, Password);
+        root.addRow(7, tf2);
+        root.addRow(9, Submit,Reset);
+  
+//setting horizontal and vertical gaps between the rows   
+        root.setHgap(10);  
+        root.setVgap(10);
+        root.setId("root-tab4");
+        return root;
     }
     
 }
